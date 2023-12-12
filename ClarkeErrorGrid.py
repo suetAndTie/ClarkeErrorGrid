8165ad54c32bf391f83324dd983a1f94988c614a
David GERARD <david.gerard@telecom-paris.fr>
2023-12-12 15:21:22 +0000

    Changed code to be able to pass and retrieve an axis Matplotlib object

--- ClarkeErrorGrid.py
+++ ClarkeErrorGrid.py
@@ -81,11 +81,15 @@ March 29 2013
 
 
 import matplotlib.pyplot as plt
+import numpy as np
 
 
 #This function takes in the reference values and the prediction values as lists and returns a list with each index corresponding to the total number
 #of points within that zone (0=A, 1=B, 2=C, 3=D, 4=E) and the plot
-def clarke_error_grid(ref_values, pred_values, title_string):
+def clarke_error_grid(ref_values, pred_values, title_string, ax=None):
+
+    if ax is None:
+        ax = plt.gca()
 
     #Checking to see if the lengths of the reference and prediction arrays are the same
     assert (len(ref_values) == len(pred_values)), "Unequal number of values (reference : {}) (prediction : {}).".format(len(ref_values), len(pred_values))
@@ -96,54 +100,52 @@ def clarke_error_grid(ref_values, pred_values, title_string):
     if min(ref_values) < 0 or min(pred_values) < 0:
         print("Input Warning: the minimum reference value {} or the minimum prediction value {} is less than 0 mg/dl.".format(min(ref_values),  min(pred_values)))
 
-    #Clear plot
-    plt.clf()
 
     #Set up plot
-    plt.scatter(ref_values, pred_values, marker='o', color='black', s=8)
-    plt.title(title_string + " Clarke Error Grid")
-    plt.xlabel("Reference Concentration (mg/dl)")
-    plt.ylabel("Prediction Concentration (mg/dl)")
-    plt.xticks([0, 50, 100, 150, 200, 250, 300, 350, 400])
-    plt.yticks([0, 50, 100, 150, 200, 250, 300, 350, 400])
-    plt.gca().set_facecolor('white')
+    ax.scatter(ref_values, pred_values, marker='o', color='black', s=8)
+    ax.set_title(title_string + " Clarke Error Grid")
+    ax.set_xlabel("Reference Concentration (mg/dl)")
+    ax.set_ylabel("Prediction Concentration (mg/dl)")
+    ax.set_xticks([0, 50, 100, 150, 200, 250, 300, 350, 400])
+    ax.set_yticks([0, 50, 100, 150, 200, 250, 300, 350, 400])
+    ax.set_facecolor('white')
 
     #Set axes lengths
-    plt.gca().set_xlim([0, 400])
-    plt.gca().set_ylim([0, 400])
-    plt.gca().set_aspect((400)/(400))
+    ax.set_xlim([0, 400])
+    ax.set_ylim([0, 400])
+    ax.set_aspect((400)/(400))
 
     #Plot zone lines
-    plt.plot([0,400], [0,400], ':', c='black')                      #Theoretical 45 regression line
-    plt.plot([0, 175/3], [70, 70], '-', c='black')
-    #plt.plot([175/3, 320], [70, 400], '-', c='black')
-    plt.plot([175/3, 400/1.2], [70, 400], '-', c='black')           #Replace 320 with 400/1.2 because 100*(400 - 400/1.2)/(400/1.2) =  20% error
-    plt.plot([70, 70], [84, 400],'-', c='black')
-    plt.plot([0, 70], [180, 180], '-', c='black')
-    plt.plot([70, 290],[180, 400],'-', c='black')
-    # plt.plot([70, 70], [0, 175/3], '-', c='black')
-    plt.plot([70, 70], [0, 56], '-', c='black')                     #Replace 175.3 with 56 because 100*abs(56-70)/70) = 20% error
-    # plt.plot([70, 400],[175/3, 320],'-', c='black')
-    plt.plot([70, 400], [56, 320],'-', c='black')
-    plt.plot([180, 180], [0, 70], '-', c='black')
-    plt.plot([180, 400], [70, 70], '-', c='black')
-    plt.plot([240, 240], [70, 180],'-', c='black')
-    plt.plot([240, 400], [180, 180], '-', c='black')
-    plt.plot([130, 180], [0, 70], '-', c='black')
+    ax.plot([0,400], [0,400], ':', c='black')                      #Theoretical 45 regression line
+    ax.plot([0, 175/3], [70, 70], '-', c='black')
+    #ax.plot([175/3, 320], [70, 400], '-', c='black')
+    ax.plot([175/3, 400/1.2], [70, 400], '-', c='black')           #Replace 320 with 400/1.2 because 100*(400 - 400/1.2)/(400/1.2) =  20% error
+    ax.plot([70, 70], [84, 400],'-', c='black')
+    ax.plot([0, 70], [180, 180], '-', c='black')
+    ax.plot([70, 290],[180, 400],'-', c='black')
+    # ax.plot([70, 70], [0, 175/3], '-', c='black')
+    ax.plot([70, 70], [0, 56], '-', c='black')                     #Replace 175.3 with 56 because 100*abs(56-70)/70) = 20% error
+    # ax.plot([70, 400],[175/3, 320],'-', c='black')
+    ax.plot([70, 400], [56, 320],'-', c='black')
+    ax.plot([180, 180], [0, 70], '-', c='black')
+    ax.plot([180, 400], [70, 70], '-', c='black')
+    ax.plot([240, 240], [70, 180],'-', c='black')
+    ax.plot([240, 400], [180, 180], '-', c='black')
+    ax.plot([130, 180], [0, 70], '-', c='black')
 
     #Add zone titles
-    plt.text(30, 15, "A", fontsize=15)
-    plt.text(370, 260, "B", fontsize=15)
-    plt.text(280, 370, "B", fontsize=15)
-    plt.text(160, 370, "C", fontsize=15)
-    plt.text(160, 15, "C", fontsize=15)
-    plt.text(30, 140, "D", fontsize=15)
-    plt.text(370, 120, "D", fontsize=15)
-    plt.text(30, 370, "E", fontsize=15)
-    plt.text(370, 15, "E", fontsize=15)
+    ax.text(30, 15, "A", fontsize=15)
+    ax.text(370, 260, "B", fontsize=15)
+    ax.text(280, 370, "B", fontsize=15)
+    ax.text(160, 370, "C", fontsize=15)
+    ax.text(160, 15, "C", fontsize=15)
+    ax.text(30, 140, "D", fontsize=15)
+    ax.text(370, 120, "D", fontsize=15)
+    ax.text(30, 370, "E", fontsize=15)
+    ax.text(370, 15, "E", fontsize=15)
 
     #Statistics from the data
-    zone = [0] * 5
+    zone = np.zeros(5)
     for i in range(len(ref_values)):
         if (ref_values[i] <= 70 and pred_values[i] <= 70) or (pred_values[i] <= 1.2*ref_values[i] and pred_values[i] >= 0.8*ref_values[i]):
             zone[0] += 1    #Zone A
@@ -157,5 +159,6 @@ def clarke_error_grid(ref_values, pred_values, title_string):
             zone[3] += 1    #Zone D
         else:
             zone[1] += 1    #Zone B
-
-    return plt, zone
+    
+    
+    return ax, zone
